cdbe7b3eb376a1e62383dab2c1fbc1d399fc6354
Ben von Huenerbein <ben.v.h@hotmail.com>
2025-07-03 11:38:24 +0200

    Added script for getting neputnes data

--- scripts/get_neptune.py
+++ scripts/get_neptune.py
@@ -12,7 +12,8 @@ def get_neptune_losses(tag):
 
     run_ids = filtered_runs["sys/id"].tolist()
 
-    results = {"replay_losses": [], "validation_losses": []}
+    replay_losses = []
+    validation_losses = []
     for run_id in run_ids:
         run = neptune.init_run(
             project="elise-neurotma/ELiSe", with_id=run_id, mode="read-only"
@@ -20,15 +21,39 @@ def get_neptune_losses(tag):
         replay_loss = run["replay_loss_r"].fetch_values()["value"].tolist()
         validation_loss = run["validation_loss_r"].fetch_values()["value"].tolist()
 
-        results["replay_losses"].append(replay_loss)
-        results["validation_losses"].append(validation_loss)
+        replay_losses.append(replay_loss)
+        validation_losses.append(validation_loss)
 
-    results["replay_losses"] = np.array(results["replay_losses"])
-    results["validation_losses"] = np.array(results["validation_losses"])
+    replay_losses = np.array(replay_losses)
+    validation_losses = np.array(validation_losses)
 
-    return results
+    return replay_losses, validation_losses, run_ids
 
 
 if __name__ == "__main__":
-    tag = "multiple_durations_150"
-    losses = get_neptune_losses(tag)
+    from pathlib import Path
+
+    tag = "silence_400_longer"
+    replay_losses, validation_losses, run_ids = get_neptune_losses(tag)
+
+    save_loc = Path(
+        "/Users/benano/Documents/org/manuscripts/SequenceLearningPaper/data/silences/"
+    )
+    save_loc_replay = save_loc / "replay_losses.npy"
+    save_loc_validation = save_loc / "validation_losses.npy"
+    save_loc_config = save_loc / "config.toml"
+
+    name = run_ids[0]
+
+    c_run = neptune.init_run(
+        project="elise-neurotma/ELiSe", with_id=name, mode="read-only"
+    )
+    c_run["parameters/config"].download(destination=str(save_loc_config))
+
+    c_run["dataloader"].download(destination=str(save_loc / "dataloader.pkl"))
+    c_run["network"].download(destination=str(save_loc / "network.pkl"))
+    c_run["train_tracker"].download(destination=str(save_loc / "train_tracker.pkl"))
+    c_run["replay_tracker"].download(destination=str(save_loc / "replay_tracker.pkl"))
+
+    np.save(save_loc_replay, replay_losses)
+    np.save(save_loc_validation, validation_losses)
