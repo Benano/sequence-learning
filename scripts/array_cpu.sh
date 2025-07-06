0ce9fcc9decce6d431d4cb92c4b9c90593a62ba3
Ben von Huenerbein <ben.v.h@hotmail.com>
2025-07-06 17:01:07 +0200

    Add slurm script

---
+++ scripts/array_cpu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name="testing slurm"
+#SBATCH --time=7:00:00
+#SBATCH --ntasks=10
+#SBATCH --array=0-2
+#SBATCH --ntasks-per-node=10
+#SBATCH --cpus-per-task=12
+#SBATCH --partition=batch
+
+# Your code below this line
+# module load Anaconda3
+# eval "$(conda shell.bash hook)"
+# conda activate elise
+# mamba activate elise
+
+NODE_ID=$SLURM_ARRAY_TASK_ID
+
+SLURM_NTASKS=${SLURM_NTASKS:-10}  # Default to 10 tasks if not set
+
+# using the SLURM_NTASKS env var
+for i in $( seq 0 $((SLURM_NTASKS-1))); do
+
+  TASK_ID=$((NODE_ID * SLURM_NTASKS + i))
+
+  if [ $TASK_ID -eq 0 ]; then
+    saving="--saving"
+  else
+      saving=""
+  fi
+    eval "python run.py --seed $TASK_ID $saving" &
+    echo "Running task with ID: $TASK_ID"
+
+done
+
+wait
