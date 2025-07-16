55660c474f2d3875a1537e5513627882bb81f4d5
Ben von Huenerbein <ben.v.h@hotmail.com>
2025-07-16 16:42:34 +0200

    Turn of learning in validation

--- scripts/train.py
+++ scripts/train.py
@@ -210,7 +210,7 @@ def main(full_config, run_path, artifact_path, pattern_path, neptune_run, rng):
         if epoch != simulation_params.training_epochs - 1:
             for t in np.arange(0, validation_duration, simulation_params.dt):
                 c_t = c_t + simulation_params.dt
-                network(u_inp=None)
+                network(u_inp=None, learn=False)
                 validation_tracker.track(network, c_t)
 
             u_out = np.array(validation_tracker["u_visible"])[-2 * len(u_target) :]
